163106ac4a30e663aa38159d95a2fa9f8f4ff502
Ahmet Oğuzperdahçı <ahmetoguzperdahci@gmail.com>
2022-12-16 20:52:40 +0300

    refactor(Entities): add exam props

--- LanguageLearningApp/LanguageLearningApp.Core/Entities/Exam.cs
+++ LanguageLearningApp/LanguageLearningApp.Core/Entities/Exam.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LanguageLearningApp.Core.Entities
 {
-    internal class Exam
+    public class Exam: IEntity
     {
+        [Key]
+        public int Id { get; set; }
+        public Student Student { get; set; }
+        public Lesson Lesson { get; set; }
     }
 }
