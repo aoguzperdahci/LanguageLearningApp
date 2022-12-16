d9d3688a0da1a43841be2bb6cad3c760f1ad5b36
Ahmet Oğuzperdahçı <ahmetoguzperdahci@gmail.com>
2022-12-16 19:55:32 +0300

    refactor(Entities): add exam and exam questions entities

---
+++ LanguageLearningApp/LanguageLearningApp.Core/Entities/ExamQuestions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageLearningApp.Core.Entities
+{
+    [PrimaryKey(nameof(Exam), nameof(QuestionNumber))]
+    public class ExamQuestions
+    {
+        public Exam Exam { get; set; }
+        public int QuestionNumber { get; set; }
+        public Question Question { get; set; }
+        public string StudentAnswer { get; set; }
+    }
+}
