d8a8b6a82f2dfbdb90fa1e26715aa081a9c6f8a1
yagmurdurukan <yagmur.durukan@arcelik.com>
2022-12-21 17:21:09 +0300

    login ve signup eklendi, düzenlenecek

--- WebClient/src/components/App/App.js
+++ WebClient/src/components/App/App.js
@@ -1,19 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavbarComponent from '../NavbarComponent/NavbarComponent';
 import ToastComponent from '../ToastComponent/ToastComponent';
+import Login from '../Login/Login'
+import SignUp from '../SignUp/SignUp'
 import './App.css';
+import Try from '../Login/Try';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <NavbarComponent />
-
+        <Login/>
+        <SignUp/>
         <Routes>
           {/* <Route index element={<HomePage />}/> */}
         </Routes>
 
-        <div style={{ height: "1000px", backgroundColor: "#EEE3CB" }}></div>
+        <div style={{ height: "1000px", backgroundColor: "#D2F1FF" }}></div>
 
         <ToastComponent />
       </div>
