2f18ab4ef3dc23309fc613220dfc8774ff83bd04
yagmurdurukan <yagmur.durukan@arcelik.com>
2022-12-24 16:02:05 +0300

    forum sayfası eklendi

--- WebClient/src/components/App/App.js
+++ WebClient/src/components/App/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ForumPage from '../Forum/ForumPage';
 import NavbarComponent from '../NavbarComponent/NavbarComponent';
 import ToastComponent from '../ToastComponent/ToastComponent';
 import './App.css';
@@ -8,7 +9,7 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <NavbarComponent />
-
+        <ForumPage/>
         <Routes>
           {/* <Route index element={<HomePage />}/> */}
         </Routes>
---
+++ WebClient/src/components/Forum/ForumPage.js
@@ -0,0 +1,27 @@
+import { Form } from 'react-bootstrap';
+import './ForumPage.css'
+
+export default function ForumPage() {
+    return (
+<div>
+            <div className="left">
+                <h1>Dersin Adı</h1>
+                <h4>Forum page</h4>
+                <div class="container">
+
+                
+                    
+                </div>
+            </div>
+            <div className='right'>
+
+
+            </div>
+        </div>
+
+       
+           
+            
+    );
+
+}
