9f04ce24afb06d92e741c81075d8b3292f611330
Ahmet Oğuzperdahçı <ahmetoguzperdahci@gmail.com>
2022-12-25 19:49:53 +0300

    fix(exception middleware): fix syntax error

--- LanguageLearningApp/LanguageLearningApp.WebAPI/ExceptionMiddleware.cs
+++ LanguageLearningApp/LanguageLearningApp.WebAPI/ExceptionMiddleware.cs
@@ -5,50 +5,47 @@ namespace LanguageLearningApp.WebAPI
 {
     public class ExceptionMiddleware
     {
-        public class ExceptionMiddleware
+        private readonly RequestDelegate next;
+
+        public ExceptionMiddleware(RequestDelegate next)
         {
-            private readonly RequestDelegate next;
+            this.next = next;
+        }
 
-            public ExceptionMiddleware(RequestDelegate next)
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
             {
-                this.next = next;
+                await next(httpContext);
             }
-
-            public async Task InvokeAsync(HttpContext httpContext)
+            catch (ArgumentNullException ex)
             {
-                try
-                {
-                    await next(httpContext);
-                }
-                catch (ArgumentNullException ex)
-                {
-                    await HandleExceptionAsync(httpContext, ex);
-                }
-                catch (Exception ex)
-                {
-                    await HandleExceptionAsync(httpContext, ex);
-                }
+                await HandleExceptionAsync(httpContext, ex);
             }
-
-            private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+            catch (Exception ex)
             {
-                context.Response.ContentType = "application/json";
-                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-
-                var message = "Internal Server Error from the custom middleware.";
-                var error = new ErrorDetails()
-                {
-                    StatusCode = context.Response.StatusCode,
-                    Message = message + Environment.NewLine + exception.Message.ToString()
-                };
-                await context.Response.WriteAsync(JsonSerializer.Serialize(error));
+                await HandleExceptionAsync(httpContext, ex);
             }
         }
 
-        public class ErrorDetails
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
         {
-            public int StatusCode { get; set; }
-            public string Message { get; set; }
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var message = "Internal Server Error from the custom middleware.";
+            var error = new ErrorDetails()
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = message + Environment.NewLine + exception.Message.ToString()
+            };
+            await context.Response.WriteAsync(JsonSerializer.Serialize(error));
         }
+    }
+
+    public class ErrorDetails
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
     }
 }
